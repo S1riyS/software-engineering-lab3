1da27b6de96e41369be165fc0bcb24810656a5b4
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2025-04-05 16:03:05 +0300

    refactor: scripts are updated

--- start-local.sh
+++ start-local.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 WILDFLY_PATH="/root/programs/wildfly-preview-34.0.0.Beta1"
-DEPLOY_PATH="$(pwd)/app/build/libs/app.war"
+DEPLOY_PATH="$(pwd)/build/lab3.war"
 
-if ./gradlew clean build; then
+if ant build; then
     echo "Deploing..."
     $WILDFLY_PATH/bin/jboss-cli.sh --connect --commands="deploy --force $DEPLOY_PATH"
     echo "Done!"
